fcc0842df0beadcd503d2dd5888fef40cb5e0886
jakelsaunders94 <jakelsaunders94@gmail.com>
2021-08-31 15:02:50 +0100

    Added tests fro TodoSerializer.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,30 +1,55 @@
 # -*- coding: utf-8 -*-
 """Serializers for Accounts app."""
 
+import django.contrib.auth.password_validation as password_validator
+from django.contrib.auth.hashers import make_password
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 from accounts.models import TodoUser
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the TodoUser model."""
+
     password = serializers.CharField(write_only=True)
 
-    def create(self, validated_data):
-        return TodoUser.objects.create(
+    def create(self, validated_data):  # noqa: D102
+        user = TodoUser(
             email=validated_data["email"],
-            password=validated_data["password"],
             first_name=validated_data["first_name"],
             last_name=validated_data["last_name"],
         )
+        user.password = make_password(validated_data["password"])
+        user.save()
 
-    def update(self, instance, validated_data):
+    def update(self, instance, validated_data):  # noqa: D102
         instance.email = validated_data.get("email", instance.email)
-        instance.password = validated_data.get("password", instance.password)
+        try:
+            password = validated_data.get("password")
+            instance.password = make_password(password)
+        except KeyError:
+            pass
         instance.first_name = validated_data.get("first_name", instance.first_name)
         instance.last_name = validated_data.get("last_name", instance.last_name)
         instance.save()
         return instance
 
-    class Meta:
+    def validate(self, data):  # noqa: D102
+        errors = dict()
+        password = data.get("password")
+        user = TodoUser(**data)
+
+        try:
+            password_validator.validate_password(password=password, user=user)
+        except ValidationError as e:
+            errors["password"] = list(e.messages)
+
+        if errors:
+            raise serializers.ValidationError(errors)
+
+        return super(UserSerializer, self).validate(data)
+
+    class Meta:  # noqa: D106
         model = TodoUser
         fields = ("id", "email", "password", "first_name", "last_name")
---
+++ accounts/tests/test_serializers.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""Tests for accounts serializers."""
+
+from unittest.mock import patch
+
+from django.contrib.auth.hashers import check_password
+from django.test import TestCase
+
+from accounts.models import TodoUser
+from accounts.serializers import UserSerializer
+
+
+class UserSerializerTests(TestCase):
+    """Tests for the user serializer."""
+
+    def setUp(self) -> None:  # noqa: D102
+        self.data = {
+            "email": "myemail@test.com",
+            "first_name": "Jake",
+            "last_name": "Saunders",
+            "password": "IAmANoodle",
+        }
+
+    def test_serialize(self):
+        """Serializer should serialize a TodoUser correctly."""
+        user = TodoUser.objects.create(
+            email="myemail@test.com", first_name="Jake", last_name="Saunders",
+        )
+        serializer = UserSerializer(user)
+        assert serializer.data == {
+            "email": "myemail@test.com",
+            "first_name": "Jake",
+            "last_name": "Saunders",
+            "id": user.id,
+        }
+
+    def test_validation_good_data(self):
+        """Serializer should de-serialize a TodoUser correctly."""
+        serializer = UserSerializer(data=self.data)
+        serializer.is_valid()
+
+        assert serializer.validated_data == self.data
+
+    @patch("accounts.serializers.password_validator.validate_password")
+    def test_validation_uses_django_password_validator(self, mock_validator):
+        """Validation should use the django standard password validator."""
+        serializer = UserSerializer(data=self.data)
+        serializer.is_valid()
+
+        mock_validator.assert_called_once()
+        assert mock_validator.call_args[1]["password"] == "IAmANoodle"
+
+    def test_password_validation_issues_are_passed_through(self):
+        """Password validator errors should be passed through to the serializer as field errors."""
+        self.data["password"] = "a"
+        serializer = UserSerializer(data=self.data)
+        is_valid = serializer.is_valid()
+
+        assert not is_valid
+        assert serializer.errors["password"] == [
+            "This password is too short. It must contain at least 8 characters.",
+            "This password is too common.",
+        ]
+
+    def test_create(self):
+        """Test create method creates an instance correctly."""
+        serializer = UserSerializer(data=self.data)
+        assert serializer.is_valid()
+        serializer.create(serializer.validated_data)
+
+        instance = TodoUser.objects.first()
+        assert instance.email == self.data["email"]
+        assert instance.first_name == self.data["first_name"]
+        assert instance.last_name == self.data["last_name"]
+        assert check_password(password=self.data["password"], encoded=instance.password)
+
+    def test_update(self):
+        """Serializer method should mutate and save user."""
+        serializer = UserSerializer(data=self.data)
+        assert serializer.is_valid()
+        serializer.create(serializer.validated_data)
+
+        new_data = {
+            "email": "new_email@test.com",
+            "first_name": "Barry",
+            "last_name": "White",
+            "password": "CantGetEnoughOfYourLoveBaby",
+        }
+
+        instance = TodoUser.objects.first()
+        serializer = UserSerializer(data=new_data)
+        assert serializer.is_valid()
+        serializer.update(instance, serializer.validated_data)
+        instance.refresh_from_db()
+
+        assert instance.email == "new_email@test.com"
+        assert instance.first_name == "Barry"
+        assert instance.last_name == "White"
+        assert check_password("CantGetEnoughOfYourLoveBaby", instance.password)
